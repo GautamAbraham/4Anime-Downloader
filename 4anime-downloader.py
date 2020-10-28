0aaaa96035ccd1791b62876f734a05667a2cb975
AllMight420 <53051082+AllMight420@users.noreply.github.com>
2020-10-28 10:51:55 +0530

    Added files

---
+++ 4anime-downloader.py
@@ -0,0 +1,42 @@
+#Only works for 4anime.to
+
+import requests
+from bs4 import BeautifulSoup
+from clint.textui import progress
+import sys
+session = requests.Session()
+
+url = input("Enter episode URL: ")
+slice_index = url.rfind("-episode-") + 9
+sliced_url = url[:slice_index]
+#use start > 0 and end > 0 and start <= end
+start = int(input("Start episode: "))
+end = int(input("End episode: "))
+filename = "Episode" 								#default filename prefix
+filename = str(input("Filename prefix: "))			#Setting filename
+if(start > 0 and end > 0 and start <= end):	
+	for i in range(start,end+1):
+		no = str(i)
+		if(len(no) == 1):
+			final_url = sliced_url + "0" + no
+		else:
+			final_url = sliced_url + no
+		print("\naccessing url : " + final_url)
+		r = session.get(final_url)					#starting webscraping
+		soup = BeautifulSoup(r.text, 'html.parser')
+		source = soup.find_all('source')
+		if(source != []):
+			img_link = source[0].get('src')
+			print("starting download - episode : " + no)
+			raw_image = session.get(img_link,stream=True)
+			total_length = int(raw_image.headers['Content-Length'])
+			with open(filename+" "+ no + '.mp4', 'wb') as f:
+			    	for c in progress.mill(raw_image.iter_content(chunk_size=1024),expected_size=(total_length/1024) +1):
+			    		if c:
+			    			f.write(c)
+			    			f.flush()
+		else:
+			print("episode not found")
+	print("\nfinished\n")
+else:
+	print("\nprovide meaningful values\n")
